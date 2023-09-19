6e51e1ee38c1d8618f0c784e2268737de8903cac
Yurii Balan <ybalan19@gmail.com>
2023-09-19 18:34:19 +0300

    isRelease should used for remote btns cleanups

--- Servo/GeneratorOff/Constants.h
+++ Servo/GeneratorOff/Constants.h
@@ -4,31 +4,31 @@
 // constants won't change
 
 //Angles
-const int GENERATOR_OFF_ANGLE = 0; //Up
-const int GENERATOR_OFF_2_ANGLE = 180; //Up
+#define GENERATOR_OFF_ANGLE 0 //Up
+#define GENERATOR_OFF_2_ANGLE 180 //Up
 
-const int INITIAL_ANGLE = 130; //Down //70; Center
-const int INITIAL_2_ANGLE = 50; //Down //110; Center
+#define INITIAL_ANGLE 130 //Down //70; Center
+#define INITIAL_2_ANGLE 50 //Down //110; Center
 
-const int START_ANGLE = 0;
-const int END_ANGLE = 130;
+#define START_ANGLE 0
+#define END_ANGLE 130
 
-const int START_2_ANGLE = 180;
-const int END_2_ANGLE = 50;
+#define START_2_ANGLE 180
+#define END_2_ANGLE 50
 
-const int ROTATE_ANGLE = 10;
-const int ROTATE_WITH_DELAY_ANGLE = 5;
+#define ROTATE_ANGLE 10
+#define ROTATE_WITH_DELAY_ANGLE 5
 
 //Delays
-const int ROTATE_DELAY = 75;
+#define ROTATE_DELAY 75
 
 //PINs
-const int GENERATOR_OFF_BTN_PIN = 7;
-const int INITIAL_POS_BTN_PIN = 6;
-const int ROTATE_RIGHT_BTN_PIN = 5;
-const int ROTATE_LEFT_BTN_PIN = 4;
-const int GENERATOR_OFF_REMOTE_BTN_PIN = 13;
-const int SERVO_PIN  = 12; //YELLOW //Left Servo
-const int SERVO2_PIN  = 11; //ORANGE //Right Servo
+#define GENERATOR_OFF_BTN_PIN 7
+#define INITIAL_POS_BTN_PIN 6
+#define ROTATE_RIGHT_BTN_PIN 5
+#define ROTATE_LEFT_BTN_PIN 4
+#define GENERATOR_OFF_REMOTE_BTN_PIN 13
+#define SERVO_PIN 12 //YELLOW //Left Servo
+#define SERVO2_PIN 11 //ORANGE //Right Servo
 
 #endif
