319c742e76910294da9e3f8bfe3514d5e1dcd018
Yurii Balan <ybalan19@gmail.com>
2024-10-06 22:40:13 +0300

    Charger: /list + excel template

--- Charger/Charger.ino
+++ Charger/Charger.ino
@@ -3,7 +3,7 @@
 #ifdef ESP8266
   #define VER F("1.0")
 #else //ESP32
-  #define VER F("1.5")
+  #define VER F("1.6")
 #endif
 
 //#define RELEASE
--- Charger/TBotMenu.h
+++ Charger/TBotMenu.h
@@ -1,7 +1,8 @@
 #pragma once
 #ifndef TELEGRAM_BOT_MENU_H
 #define TELEGRAM_BOT_MENU_H
-//#include <math.h>
+
+#include <math.h>
 #include "DEBUGHelper.h"
 #include "Settings.h"
 
@@ -37,27 +38,34 @@ notify0 - Notifications Off
 notify1 - Notify All http code result
 notifynot200 - Notify all http code exclude OK(200)
 notify429 - Notify To-many Requests (429)
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CUSTOM
+
 */
 
-#define BOT_COMMAND_RESTART F("/restart")
-#define BOT_COMMAND_TEST F("/test")
-#define BOT_COMMAND_VER F("/ver")
+#define BOT_COMMAND_RESTART       F("/restart")
+#define BOT_COMMAND_TEST          F("/test")
+#define BOT_COMMAND_VER           F("/ver")
 #define BOT_COMMAND_CHANGE_CONFIG F("/changeconfig")
-#define BOT_COMMAND_MENU F("/menu")
-#define BOT_COMMAND_UPDATE F("/update")
-#define BOT_COMMAND_TOKEN F("/token")
-#define BOT_COMMAND_NSTAT F("/nstat")
-#define BOT_COMMAND_RSSI F("/rssi")
-#define BOT_COMMAND_CHID F("/chid")
-#define BOT_COMMAND_FS F("/fs")
-#define BOT_COMMAND_DOWNLOAD F("/download")
-
-//Fast Menu
+#define BOT_COMMAND_MENU          F("/menu")
+#define BOT_COMMAND_UPDATE        F("/update")
+#define BOT_COMMAND_TOKEN         F("/token")
+#define BOT_COMMAND_NSTAT         F("/nstat")
+#define BOT_COMMAND_RSSI          F("/rssi")
+#define BOT_COMMAND_CHID          F("/chid")
+#define BOT_COMMAND_FS            F("/fs")
 
 //Notify
 #ifdef USE_NOTIFY
 #define BOT_COMMAND_NOTIFY F("/notify")
 #endif
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CUSTOM
+#define BOT_COMMAND_DOWNLOAD      F("/get")
+#define BOT_COMMAND_REMOVE        F("/rem")
+#define BOT_COMMAND_LIST          F("/ls")
+
+
+//Fast Menu
 
 void PrintNetworkStatistic(String &str, const int& codeFilter);
 const String GetPMMenu(const float &voltage, const String &chatId, const float& led_consumption_voltage_factor = 0.0);
@@ -154,7 +162,69 @@ const std::vector<String> HandleBotMenu(FB_msg& msg, String &filtered, const boo
       BOT_MENU_INFO(F("\t"), channel.first);
       value += String(F("[")) + channel.first + F("]") + F("; ");
     }
-  }else
+  }
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CUSTOM  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  else if(GetCommandValue(BOT_COMMAND_LIST, filtered, value))
+  {
+    BOT_MENU_INFO(F("BOT LIST:"));
+    bot->sendTyping(msg.chatID);
+
+    const size_t &last = value.toInt();
+
+    String filter;
+    int totalRecordsCount = 0;
+    uint32_t totalSize = 0;
+    auto filesInfo = ds->downloadData(filter, totalRecordsCount, totalSize); 
+
+    BOT_MENU_TRACE(F("Records: "), totalRecordsCount, F(" "), F("Total size: "), totalSize);
+    if(filesInfo.size() > 0)
+    {
+      std::vector<String> files;
+      for(const auto& fi : filesInfo)
+        files.push_back(fi.first);
+
+      // Sort vector in descending order using a lambda function as comparator
+      std::sort(files.begin(), files.end(), [](const String& a, const String& b) {
+          return a > b; // Descending order
+      });     
+
+      value.clear();
+      value += ds->startDate + F(" - ") + ds->endDate + F(" : ") + F("(") + F("Records: ") + String(totalRecordsCount) + F(" ") + F("Total size: ") + String(totalSize) + F(")");
+      messages.push_back(value); value.clear();
+      value += F("\n");                                                       // NewLine
+
+      String endDateCmd = ds->endDate; endDateCmd.replace('-', '_');
+
+      value += String(F("Download All: ")) + BOT_COMMAND_DOWNLOAD + endDateCmd.substring(0, 4);
+      messages.push_back(value); value.clear();
+      value += F("\n");                                                       // NewLine
+
+      value += String(F("Download Last Month: ")) + BOT_COMMAND_DOWNLOAD + endDateCmd.substring(0, 7);
+      messages.push_back(value); value.clear();
+      value += F("\n");                                                       // NewLine
+
+      const size_t &num = last > 0 ? min(files.size(), last) : files.size();
+      for(uint8_t idx = 0; idx < num; idx++)
+      {
+        const String &fileName = files[idx];
+        String fileNameCmd = fileName; fileNameCmd.replace('-', '_');
+
+        value +=
+                fileName.substring(0, fileName.length() - FILE_EXT_LEN)         // fileName without extension
+                + F("(") + filesInfo[fileName].linesCount + F(")")              // + lines count
+                + F(" ")                                                        // delimeter
+                + F("(") + BOT_COMMAND_DOWNLOAD + fileNameCmd + F(")")          // /download or /get
+                + F(" ")                                                        // delimeter
+                + F("(") + BOT_COMMAND_REMOVE + fileNameCmd + F(")")            // /remove or /rem                
+        ;
+        messages.push_back(value); value.clear();
+        value += F("\n");                                                       // NewLine        
+      }
+
+    }else
+      value = F("Files not found");
+  }
+  else
   if(GetCommandValue(BOT_COMMAND_DOWNLOAD, filtered, value))
   {
     BOT_MENU_INFO(F("BOT DOWNLOAD:"));
@@ -168,11 +238,7 @@ const std::vector<String> HandleBotMenu(FB_msg& msg, String &filtered, const boo
     BOT_MENU_TRACE(F("Records Count: "), totalRecordsCount, F(" "), F("Total size: "), totalSize);
     if(filesInfo.size() > 0)
     {
-      //const auto &totalRecordsCount = TelegramBot::getFilesLineCount(filesInfo);
-      String outerFileName = filter + F("(") + totalRecordsCount + F(")") + FILE_EXT;         
-
-      filter = String(FILE_PATH) + F("/") + filter;
-      BOT_MENU_TRACE(filter, F(" -> "), outerFileName);
+      filter = String(FILE_PATH) + F("/") + filter;      
 
       std::vector<String> files;
       for(const auto& fi : filesInfo)
@@ -181,12 +247,12 @@ const std::vector<String> HandleBotMenu(FB_msg& msg, String &filtered, const boo
       // Sort vector in descending order using a lambda function as comparator
       std::sort(files.begin(), files.end(), [](const String& a, const String& b) {
           return a < b; // Ascending order
-      });
-
-      // File f = SPIFFS.open(filter.c_str());
-      // bot->sendFile(f, FB_DOC, outerFileName,  msg.chatID);
-      // f.close();    
+      });     
       
+      //const auto &totalRecordsCount = TelegramBot::getFilesLineCount(filesInfo);
+      String outerFileName = filter + F("(") + totalRecordsCount + F(")") + FILE_EXT; 
+      BOT_MENU_TRACE(filter, F(" -> "), outerFileName);
+
       if(bot->sendFile(files, totalSize, outerFileName, msg.chatID) != 1)
       {
         value = F("Telegram error");
@@ -194,7 +260,7 @@ const std::vector<String> HandleBotMenu(FB_msg& msg, String &filtered, const boo
       else
         value.clear();
     }else
-      value = F("files not found");
+      value = F("Files not found");
   }
   
 
